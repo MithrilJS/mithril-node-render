57f56d2d8ecdab931c2ab8463953c3fb3fc72f28
Stephan Hoyer <ste.hoyer@gmail.com>
2016-05-20 22:52:37 +0200

    Standard.js

--- index.js
+++ index.js
@@ -1,136 +1,136 @@
-'use strict';
+'use strict'
 
 var VOID_TAGS = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr',
-    'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track',
-    'wbr', '!doctype'];
+  'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track',
+  'wbr', '!doctype']
 
-function isArray(thing) {
-  return Object.prototype.toString.call(thing) === '[object Array]';
+function isArray (thing) {
+  return Object.prototype.toString.call(thing) === '[object Array]'
 }
 
-function camelToDash(str) {
+function camelToDash (str) {
   return str.replace(/\W+/g, '-')
-            .replace(/([a-z\d])([A-Z])/g, '$1-$2');
+    .replace(/([a-z\d])([A-Z])/g, '$1-$2')
 }
 
-function removeEmpties(n) {
-  return n != '';
+function removeEmpties (n) {
+  return n !== ''
 }
 
 // shameless stolen from https://github.com/punkave/sanitize-html
-function escapeHtml(s, replaceDoubleQuote) {
+function escapeHtml (s, replaceDoubleQuote) {
   if (s === 'undefined') {
-    s = '';
+    s = ''
   }
-  if (typeof(s) !== 'string') {
-    s = s + '';
+  if (typeof (s) !== 'string') {
+    s = s + ''
   }
-  s =  s.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/\>/g, '&gt;');
+  s = s.replace(/\&/g, '&amp;').replace(/</g, '&lt;').replace(/\>/g, '&gt;')
   if (replaceDoubleQuote) {
-    return s.replace(/\"/g, '&quot;');
+    return s.replace(/\"/g, '&quot;')
   }
-  return s;
+  return s
 }
 
-function createAttrString(view, escapeAttributeValue) {
-  var attrs = view.attrs;
-  
+function createAttrString (view, escapeAttributeValue) {
+  var attrs = view.attrs
+
   if (!attrs || !Object.keys(attrs).length) {
-    return '';
+    return ''
   }
 
-  return Object.keys(attrs).map(function(name) {
-    var value = attrs[name];
+  return Object.keys(attrs).map(function (name) {
+    var value = attrs[name]
     if (typeof value === 'undefined' || value === null || typeof value === 'function') {
-      return;
+      return
     }
     if (typeof value === 'boolean') {
-      return value ? ' ' + name : '';
+      return value ? ' ' + name : ''
     }
     if (name === 'style') {
       if (!value) {
-        return;
+        return
       }
-      var styles = attrs.style;
+      var styles = attrs.style
       if (typeof styles === 'object') {
-        styles = Object.keys(styles).map(function(property) {
-          return styles[property] != '' ? [camelToDash(property).toLowerCase(), styles[property]].join(':') : '';
-        }).filter(removeEmpties).join(';');
+        styles = Object.keys(styles).map(function (property) {
+          return styles[property] !== '' ? [camelToDash(property).toLowerCase(), styles[property]].join(':') : ''
+        }).filter(removeEmpties).join(';')
       }
-      return styles != '' ? ' style="' + escapeAttributeValue(styles, true) + '"' : '';
+      return styles !== '' ? ' style="' + escapeAttributeValue(styles, true) + '"' : ''
     }
-    
+
     // Handle SVG <use> tags specially
     if (name === 'href' && view.tag === 'use') {
-        return ' xlink:href="' + escapeAttributeValue(value, true) + '"';
+      return ' xlink:href="' + escapeAttributeValue(value, true) + '"'
     }
-    
-    return ' ' + (name === 'className' ? 'class' : name) + '="' + escapeAttributeValue(value, true) + '"';
-  }).join('');
+
+    return ' ' + (name === 'className' ? 'class' : name) + '="' + escapeAttributeValue(value, true) + '"'
+  }).join('')
 }
 
-function createChildrenContent(view) {
-  if(isArray(view.children) && !view.children.length) {
-    return '';
+function createChildrenContent (view) {
+  if (isArray(view.children) && !view.children.length) {
+    return ''
   }
 
-  return render(view.children);
+  return render(view.children)
 }
 
-function render(view, options) {
-  options = options || {};
+function render (view, options) {
+  options = options || {}
 
   var defaultOptions = {
     escapeAttributeValue: escapeHtml,
     escapeString: escapeHtml
-  };
+  }
 
-  Object.keys(defaultOptions).forEach(function(key) {
-    if (!options.hasOwnProperty(key)) options[key] = defaultOptions[key];
-  });
+  Object.keys(defaultOptions).forEach(function (key) {
+    if (!options.hasOwnProperty(key)) options[key] = defaultOptions[key]
+  })
 
-  var type = typeof view;
+  var type = typeof view
 
   if (type === 'string') {
-    return options.escapeString(view);
+    return options.escapeString(view)
   }
 
   if (type === 'number' || type === 'boolean') {
-    return view;
+    return view
   }
 
   if (!view) {
-    return '';
+    return ''
   }
 
   if (isArray(view)) {
-    return view.map(function(view) { return render(view, options) }).join('');
+    return view.map(function (view) { return render(view, options) }).join('')
   }
 
-  //compontent
+  // compontent
   if (view.view) {
-    var scope = view.controller ? new view.controller : {};
-    var result = render(view.view(scope), options);
+    var scope = view.controller ? new view.controller() : {}
+    var result = render(view.view(scope), options)
     if (scope.onunload) {
-      scope.onunload();
+      scope.onunload()
     }
-    return result;
+    return result
   }
 
   if (view.$trusted) {
-    return '' + view;
+    return '' + view
   }
-  var children = createChildrenContent(view);
+  var children = createChildrenContent(view)
   if (!children && VOID_TAGS.indexOf(view.tag.toLowerCase()) >= 0) {
-    return '<' + view.tag + createAttrString(view, options.escapeAttributeValue) + '>';
+    return '<' + view.tag + createAttrString(view, options.escapeAttributeValue) + '>'
   }
   return [
     '<', view.tag, createAttrString(view, options.escapeAttributeValue), '>',
     children,
-    '</', view.tag, '>',
-  ].join('');
+    '</', view.tag, '>'
+  ].join('')
 }
 
-render.escapeHtml = escapeHtml;
+render.escapeHtml = escapeHtml
 
-module.exports = render;
+module.exports = render
--- test.js
+++ test.js
@@ -1,108 +1,107 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var m = require('mithril');
-var render = require('./index');
+var test = require('tape')
+var m = require('mithril')
+var render = require('./index')
 
-test('render', function(t) {
+test('render', function (t) {
   t.equal(render(m('span', 'content')),
-    '<span>content</span>', 'should render tag');
+    '<span>content</span>', 'should render tag')
   t.equal(render(m('.foo', 'content')),
-    '<div class="foo">content</div>', 'should render classname');
+    '<div class="foo">content</div>', 'should render classname')
   t.equal(render(m('#bar', 'content')),
-    '<div id="bar">content</div>', 'should render id');
-  t.equal(render(m('br')), '<br>', 'should render short nodes when no children');
-  t.equal(render(m('HR')), '<HR>', 'should render short nodes when no children and tag name is uppercase');
+    '<div id="bar">content</div>', 'should render id')
+  t.equal(render(m('br')), '<br>', 'should render short nodes when no children')
+  t.equal(render(m('HR')), '<HR>', 'should render short nodes when no children and tag name is uppercase')
   t.equal(render(m('!doctype')), '<!doctype>',
-    'should render short node doctype');
+    'should render short node doctype')
   t.equal(render(m('!doctype', {html: true})), '<!doctype html>',
-    'should render short node doctype HTML5');
+    'should render short node doctype HTML5')
   t.equal(render(m('span', {
     'data-foo': 'bar',
     selected: 'selected'
-  })), '<span data-foo="bar" selected="selected"></span>', 'should render attributes');
-  t.equal(render(m('ul', 'huhu')), '<ul>huhu</ul>', 'should render string');
+  })), '<span data-foo="bar" selected="selected"></span>', 'should render attributes')
+  t.equal(render(m('ul', 'huhu')), '<ul>huhu</ul>', 'should render string')
   t.equal(render([m('span', 'foo'), m('div', 'bar')]),
-    '<span>foo</span><div>bar</div>', 'should render arrays');
-  t.equal(render(m('span', m('div'))), '<span><div></div></span>', 'should render children');
+    '<span>foo</span><div>bar</div>', 'should render arrays')
+  t.equal(render(m('span', m('div'))), '<span><div></div></span>', 'should render children')
   t.equal(render(m('span', {
-    onmousemove: function(event) {}
-  })), '<span></span>', 'should not render events');
+    onmousemove: function (event) {}
+  })), '<span></span>', 'should not render events')
   t.equal(render(m('span', {
     style: {
       paddingLeft: '10px',
       color: 'red'
     }
-  })), '<span style="padding-left:10px;color:red"></span>', 'should render children');
+  })), '<span style="padding-left:10px;color:red"></span>', 'should render children')
   t.equal(render(m('div', [
     1,
     m('span'),
-    "2"
-  ])), '<div>1<span></span>2</div>', 'should render numbers as text nodes');
-  t.equal(render(m('div', 0)), '<div>0</div>');
-  t.equal(render(m('div', false)), '<div>false</div>');
-  t.equal(render(m('div', { a: true})), '<div a></div>');
-  t.equal(render(m('div', { a: false})), '<div></div>');
-  t.equal(render(m('div', { a: undefined})), '<div></div>');
-  t.equal(render(m('div', { style: null })), '<div></div>');
-  t.equal(render(m('div', { style: '' })), '<div></div>');
-  t.equal(render(m('div', { style: { color: '' } })), '<div></div>');
-  t.equal(render(m('div', { style: { height: '20px', color: '' } })), '<div style="height:20px"></div>');
-  t.equal(render(m('div', { style: { height: '20px', color: '', width: '10px' } })), '<div style="height:20px;width:10px"></div>');
-  t.equal(render(m('div', { a: 'foo'})), '<div a="foo"></div>');
-  t.equal(render(m('div', m.trust('<foo></foo>'))), '<div><foo></foo></div>');
-  t.equal(render(m('div', '<foo></foo>')), '<div>&lt;foo&gt;&lt;/foo&gt;</div>');
+    '2'
+  ])), '<div>1<span></span>2</div>', 'should render numbers as text nodes')
+  t.equal(render(m('div', 0)), '<div>0</div>')
+  t.equal(render(m('div', false)), '<div>false</div>')
+  t.equal(render(m('div', { a: true })), '<div a></div>')
+  t.equal(render(m('div', { a: false })), '<div></div>')
+  t.equal(render(m('div', { a: undefined })), '<div></div>')
+  t.equal(render(m('div', { style: null })), '<div></div>')
+  t.equal(render(m('div', { style: '' })), '<div></div>')
+  t.equal(render(m('div', { style: { color: '' } })), '<div></div>')
+  t.equal(render(m('div', { style: { height: '20px', color: '' } })), '<div style="height:20px"></div>')
+  t.equal(render(m('div', { style: { height: '20px', color: '', width: '10px' } })), '<div style="height:20px;width:10px"></div>')
+  t.equal(render(m('div', { a: 'foo' })), '<div a="foo"></div>')
+  t.equal(render(m('div', m.trust('<foo></foo>'))), '<div><foo></foo></div>')
+  t.equal(render(m('div', '<foo></foo>')), '<div>&lt;foo&gt;&lt;/foo&gt;</div>')
   t.equal(render(m('div', {
     style: '"></div><div a="'
-  })), '<div style="&quot;&gt;&lt;/div&gt;&lt;div a=&quot;"></div>');
+  })), '<div style="&quot;&gt;&lt;/div&gt;&lt;div a=&quot;"></div>')
   t.equal(render(m('div', {
     style: '"></div><div a="'
-  }), { escapeAttributeValue: function(value) { return value; } }), '<div style=""></div><div a=""></div>');
-  t.equal('function', typeof render.escapeHtml);
-  t.equal(render(m('pre', 'var = ' + JSON.stringify({foo: 1}))), '<pre>var = {"foo":1}</pre>');
-  t.equal(render(m('svg', m('use', { href : "fooga.com" }))), '<svg><use xlink:href="fooga.com"></use></svg>');
-  t.end();
-});
+  }), { escapeAttributeValue: function (value) { return value } }), '<div style=""></div><div a=""></div>')
+  t.equal('function', typeof render.escapeHtml)
+  t.equal(render(m('pre', 'var = ' + JSON.stringify({foo: 1}))), '<pre>var = {"foo":1}</pre>')
+  t.equal(render(m('svg', m('use', { href: 'fooga.com' }))), '<svg><use xlink:href="fooga.com"></use></svg>')
+  t.end()
+})
 
-test('components', function(t) {
-  var events = {};
+test('components', function (t) {
+  var events = {}
   var myComponent = {
-    controller: function(data) {
+    controller: function (data) {
       return {
         foo: 'bar',
         onunload: events.onunload
-      };
+      }
     },
-    view: function(scope, data) {
+    view: function (scope, data) {
       return m('div', [
         'hello',
         scope.foo,
         data
-      ]);
+      ])
     }
-  };
-  t.equal(render(m('div', myComponent)), '<div><div>hellobar</div></div>');
-  t.equal(render(m('span', m.component(myComponent, 'baz'))), '<span><div>hellobarbaz</div></span>');
-  events.onunload = t.end;
-  t.equal(render(m('span', m(myComponent, 'foz'))), '<span><div>hellobarfoz</div></span>');
+  }
+  t.equal(render(m('div', myComponent)), '<div><div>hellobar</div></div>')
+  t.equal(render(m('span', m.component(myComponent, 'baz'))), '<span><div>hellobarbaz</div></span>')
+  events.onunload = t.end
+  t.equal(render(m('span', m(myComponent, 'foz'))), '<span><div>hellobarfoz</div></span>')
   t.equal(render(m('div', {
-    controller: function() {
-      this.foo = 'bar';
+    controller: function () {
+      this.foo = 'bar'
     },
-    view: function(ctrl) {
-      return m('span', ctrl.foo);
+    view: function (ctrl) {
+      return m('span', ctrl.foo)
     }
-  })), '<div><span>bar</span></div>');
+  })), '<div><span>bar</span></div>')
   t.equal(render(m('div', {
-    controller: function() {
-    },
-    view: function() {
-      return m('span', 'huhu');
+    controller: function () {},
+    view: function () {
+      return m('span', 'huhu')
     }
-  })), '<div><span>huhu</span></div>');
+  })), '<div><span>huhu</span></div>')
   t.equal(render(m('div', {
-    view: function() {
-      return m('span', 'huhu');
+    view: function () {
+      return m('span', 'huhu')
     }
-  })), '<div><span>huhu</span></div>');
-});
+  })), '<div><span>huhu</span></div>')
+})
