a6b162130939bb84745e90f4204ca2a4a753600c
Stephan Hoyer <ste.hoyer@gmail.com>
2016-05-25 23:09:09 +0200

    Update to mithril 1.0.0

--- test.js
+++ test.js
@@ -1,107 +1,77 @@
 'use strict'
 
-var test = require('tape')
-var m = require('mithril')
+var o = require('mithril/ospec/ospec')
+var m = require('mithril/render/hyperscript')
+var mTrust = require('mithril/render/trust')
 var render = require('./index')
 
-test('render', function (t) {
-  t.equal(render(m('span', 'content')),
-    '<span>content</span>', 'should render tag')
-  t.equal(render(m('.foo', 'content')),
-    '<div class="foo">content</div>', 'should render classname')
-  t.equal(render(m('#bar', 'content')),
-    '<div id="bar">content</div>', 'should render id')
-  t.equal(render(m('br')), '<br>', 'should render short nodes when no children')
-  t.equal(render(m('HR')), '<HR>', 'should render short nodes when no children and tag name is uppercase')
-  t.equal(render(m('!doctype')), '<!doctype>',
-    'should render short node doctype')
-  t.equal(render(m('!doctype', {html: true})), '<!doctype html>',
-    'should render short node doctype HTML5')
-  t.equal(render(m('span', {
-    'data-foo': 'bar',
-    selected: 'selected'
-  })), '<span data-foo="bar" selected="selected"></span>', 'should render attributes')
-  t.equal(render(m('ul', 'huhu')), '<ul>huhu</ul>', 'should render string')
-  t.equal(render([m('span', 'foo'), m('div', 'bar')]),
-    '<span>foo</span><div>bar</div>', 'should render arrays')
-  t.equal(render(m('span', m('div'))), '<span><div></div></span>', 'should render children')
-  t.equal(render(m('span', {
-    onmousemove: function (event) {}
-  })), '<span></span>', 'should not render events')
-  t.equal(render(m('span', {
-    style: {
-      paddingLeft: '10px',
-      color: 'red'
-    }
-  })), '<span style="padding-left:10px;color:red"></span>', 'should render children')
-  t.equal(render(m('div', [
-    1,
-    m('span'),
-    '2'
-  ])), '<div>1<span></span>2</div>', 'should render numbers as text nodes')
-  t.equal(render(m('div', 0)), '<div>0</div>')
-  t.equal(render(m('div', false)), '<div>false</div>')
-  t.equal(render(m('div', { a: true })), '<div a></div>')
-  t.equal(render(m('div', { a: false })), '<div></div>')
-  t.equal(render(m('div', { a: undefined })), '<div></div>')
-  t.equal(render(m('div', { style: null })), '<div></div>')
-  t.equal(render(m('div', { style: '' })), '<div></div>')
-  t.equal(render(m('div', { style: { color: '' } })), '<div></div>')
-  t.equal(render(m('div', { style: { height: '20px', color: '' } })), '<div style="height:20px"></div>')
-  t.equal(render(m('div', { style: { height: '20px', color: '', width: '10px' } })), '<div style="height:20px;width:10px"></div>')
-  t.equal(render(m('div', { a: 'foo' })), '<div a="foo"></div>')
-  t.equal(render(m('div', m.trust('<foo></foo>'))), '<div><foo></foo></div>')
-  t.equal(render(m('div', '<foo></foo>')), '<div>&lt;foo&gt;&lt;/foo&gt;</div>')
-  t.equal(render(m('div', {
-    style: '"></div><div a="'
-  })), '<div style="&quot;&gt;&lt;/div&gt;&lt;div a=&quot;"></div>')
-  t.equal(render(m('div', {
-    style: '"></div><div a="'
-  }), { escapeAttributeValue: function (value) { return value } }), '<div style=""></div><div a=""></div>')
-  t.equal('function', typeof render.escapeHtml)
-  t.equal(render(m('pre', 'var = ' + JSON.stringify({foo: 1}))), '<pre>var = {"foo":1}</pre>')
-  t.equal(render(m('svg', m('use', { href: 'fooga.com' }))), '<svg><use xlink:href="fooga.com"></use></svg>')
-  t.end()
+o('render', function () {
+  o(render(m('span', 'content'))).equals('<span>content</span>')('should render tag')
+  o(render(m('.foo', 'content'))).equals('<div class="foo">content</div>')('should render classname')
+  o(render(m('#bar', 'content'))).equals('<div id="bar">content</div>')('should render id')
+  o(render(m('br'))).equals('<br>')('should render short nodes when no children')
+  o(render(m('HR'))).equals('<HR>')('should render short nodes when no children and tag name is uppercase')
+  o(render(m('!doctype'))).equals('<!doctype>')('should render short node doctype')
+  o(render(m('!doctype', {html: true}))).equals('<!doctype html>')('should render short node doctype HTML5')
+  o(render(m('span', { 'data-foo': 'bar', selected: 'selected' }))).equals('<span data-foo="bar" selected="selected"></span>')('should render attributes')
+  o(render(m('ul', 'huhu'))).equals('<ul>huhu</ul>')('should render string')
+  o(render([m('span', 'foo'), m('div', 'bar')])).equals('<span>foo</span><div>bar</div>')('should render arrays')
+  o(render(m('span', m('div')))).equals('<span><div></div></span>')('should render children')
+  o(render(m('span', { onmousemove: function (event) {} }))).equals('<span></span>')('should not render events')
+  o(render(m('span', { style: { paddingLeft: '10px', color: 'red' } }))).equals('<span style="padding-left:10px;color:red"></span>')('should render children')
+  o(render(m('div', [ 1, m('span'), '2' ]))).equals('<div>1<span></span>2</div>')('should render numbers as text nodes')
+  o(render(m('div', 0))).equals('<div>0</div>')
+  o(render(m('div', false))).equals('<div>false</div>')
+  o(render(m('div', { a: true }))).equals('<div a></div>')
+  o(render(m('div', { a: false }))).equals('<div></div>')
+  o(render(m('div', { a: undefined }))).equals('<div></div>')
+  o(render(m('div', { style: null }))).equals('<div></div>')
+  o(render(m('div', { style: '' }))).equals('<div></div>')
+  o(render(m('div', { style: { color: '' } }))).equals('<div></div>')
+  o(render(m('div', { style: { height: '20px', color: '' } }))).equals('<div style="height:20px"></div>')
+  o(render(m('div', { style: { height: '20px', color: '', width: '10px' } }))).equals('<div style="height:20px;width:10px"></div>')
+  o(render(m('div', { a: 'foo' }))).equals('<div a="foo"></div>')
+  o(render(m('div', mTrust('<foo></foo>')))).equals('<div><foo></foo></div>')
+  o(render(m('div', '<foo></foo>'))).equals('<div>&lt;foo&gt;&lt;/foo&gt;</div>')
+  o(render(m('div', { style: '"></div><div a="' }))).equals('<div style="&quot;&gt;&lt;/div&gt;&lt;div a=&quot;"></div>')
+  o(render(m('div', { style: '"></div><div a="' }), { escapeAttributeValue: function (value) { return value } })).equals('<div style=""></div><div a=""></div>')
+  o(typeof render.escapeHtml).equals('function')
+  o(render(m('pre', 'var = ' + JSON.stringify({foo: 1})))).equals('<pre>var = {"foo":1}</pre>')
+  o(render(m('svg', m('use', { href: 'fooga.com' })))).equals('<svg><use xlink:href="fooga.com"></use></svg>')
 })
 
-test('components', function (t) {
-  var events = {}
+o('components', function () {
+  var onremove = o.spy()
   var myComponent = {
-    controller: function (data) {
-      return {
-        foo: 'bar',
-        onunload: events.onunload
+    oninit: function (node) {
+      node.state = {
+        foo: 'bar'
       }
     },
-    view: function (scope, data) {
+    onremove: onremove,
+    view: function (node) {
       return m('div', [
         'hello',
-        scope.foo,
-        data
+        node.state.foo,
+        node.attrs.foo
       ])
     }
   }
-  t.equal(render(m('div', myComponent)), '<div><div>hellobar</div></div>')
-  t.equal(render(m('span', m.component(myComponent, 'baz'))), '<span><div>hellobarbaz</div></span>')
-  events.onunload = t.end
-  t.equal(render(m('span', m(myComponent, 'foz'))), '<span><div>hellobarfoz</div></span>')
-  t.equal(render(m('div', {
-    controller: function () {
-      this.foo = 'bar'
-    },
-    view: function (ctrl) {
-      return m('span', ctrl.foo)
-    }
-  })), '<div><span>bar</span></div>')
-  t.equal(render(m('div', {
-    controller: function () {},
+  o(onremove.callCount).equals(0)
+  o(render(m('div', m(myComponent)))).equals('<div><div>hellobar</div></div>')
+  o(onremove.callCount).equals(1)
+  o(render(m('span', m(myComponent, {foo: 'foz'})))).equals('<span><div>hellobarfoz</div></span>')
+  o(render(m('div', m({
+    oninit: function () {},
     view: function () {
       return m('span', 'huhu')
     }
-  })), '<div><span>huhu</span></div>')
-  t.equal(render(m('div', {
+  })))).equals('<div><span>huhu</span></div>')
+  o(render(m('div', m({
     view: function () {
       return m('span', 'huhu')
     }
-  })), '<div><span>huhu</span></div>')
+  })))).equals('<div><span>huhu</span></div>')
 })
+
+o.run()
